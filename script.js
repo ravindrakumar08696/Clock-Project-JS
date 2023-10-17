ca205bae316bfde9a85ee7466db83a7c67811c85
ravindrakumar08696 <119472779+ravindrakumar08696@users.noreply.github.com>
2023-10-17 14:42:47 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let date= new Date();
+console.log(date);
+let h= date.getHours();
+let m= date.getMinutes();
+let s= date.getSeconds();
+console.log(h+":"+m+":"+s);
+setInterval=(function1,1000);
+
+
+
